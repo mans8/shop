48095a1d5b752fa74968d19067082f4d8d3dccdf
小新哥 <403713133@qq.com>
2020-10-11 15:35:13 +0800

    首次提交

---
+++ shop-gateway/src/test/java/com/hgx/shop/gateway/ShopGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgx.shop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
