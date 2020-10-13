f8551271dfb749f9a585feb3d5d4ce230019c207
小新哥 <403713133@qq.com>
2020-10-13 18:28:27 +0800

    递归获取树商品分类菜单形

--- shop-product/src/main/java/com/hgx/shop/product/controller/CategoryController.java
+++ shop-product/src/main/java/com/hgx/shop/product/controller/CategoryController.java
@@ -1,21 +1,16 @@
 package com.hgx.shop.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-
+import com.hgx.common.utils.R;
+import com.hgx.shop.product.entity.CategoryEntity;
+import com.hgx.shop.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hgx.shop.product.entity.CategoryEntity;
-import com.hgx.shop.product.service.CategoryService;
-import com.hgx.common.utils.PageUtils;
-import com.hgx.common.utils.R;
-
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -32,14 +27,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 列表，查出所有分类，父子树
      */
-    @RequestMapping("/list")
-    //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
     }
 
 
--- shop-product/src/main/java/com/hgx/shop/product/service/impl/CategoryServiceImpl.java
+++ shop-product/src/main/java/com/hgx/shop/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.hgx.shop.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +28,50 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 获取所有菜单
+     *
+     * @return 所有菜单并组成树形结构
+     */
+    @Override
+    public List<CategoryEntity> listWithTree() {
+
+        //1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //2.找到所有一级分类并组成树形结构
+        return entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu) -> {
+            menu.setChildren(getChildrens(menu, entities));
+            return menu;
+        }).sorted((menu1, menu2) -> {
+            return menu1.getSort() - menu2.getSort();
+        }).collect(Collectors.toList());
+    }
+
+    /**
+     * 获取父菜单下的所有子菜单
+     *
+     * @param root 父菜单
+     * @param all  需要遍历的所有菜单
+     * @return 父菜单下所有子菜单并组成树形结构
+     */
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all) {
+
+        return all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity -> {
+            //1.递归查找子菜单
+            categoryEntity.setChildren(getChildrens(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> {
+            //2.菜单排序
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+
     }
 
 }
