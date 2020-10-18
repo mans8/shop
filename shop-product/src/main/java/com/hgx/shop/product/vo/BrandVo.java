b2f6b11c44c3b3d72babadb39bb5c380e180ffed
小新哥 <403713133@qq.com>
2020-10-19 00:42:57 +0800

    首次提交

---
+++ shop-product/src/main/java/com/hgx/shop/product/vo/BrandVo.java
@@ -0,0 +1,15 @@
+package com.hgx.shop.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author hgx
+ * @date 2020/10/18 13:49
+ * @Description
+ */
+@Data
+public class BrandVo {
+
+    private Long brandId;
+    private String brandName;
+}
