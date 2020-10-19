c3edcd647420f15eb55f7dc1e0932d4e000f08ac
小新哥 <403713133@qq.com>
2020-10-20 00:33:31 +0800

    增加查询库存功能

--- shop-ware/src/main/java/com/hgx/shop/ware/service/impl/PurchaseDetailServiceImpl.java
+++ shop-ware/src/main/java/com/hgx/shop/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,7 +1,9 @@
 package com.hgx.shop.ware.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +13,7 @@ import com.hgx.common.utils.Query;
 import com.hgx.shop.ware.dao.PurchaseDetailDao;
 import com.hgx.shop.ware.entity.PurchaseDetailEntity;
 import com.hgx.shop.ware.service.PurchaseDetailService;
+import org.springframework.util.StringUtils;
 
 
 @Service("purchaseDetailService")
@@ -18,9 +21,27 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<PurchaseDetailEntity> queryWrapper = new QueryWrapper<PurchaseDetailEntity>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.and(w -> {
+                w.eq("purchase_id", key).or().eq("sku_id", key);
+            });
+        }
+
+        String status = (String) params.get("status");
+        if (!StringUtils.isEmpty(status)) {
+            queryWrapper.eq("status", status);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)) {
+            queryWrapper.eq("ware_id",wareId);
+        }
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
--- shop-ware/src/main/java/com/hgx/shop/ware/service/impl/WareSkuServiceImpl.java
+++ shop-ware/src/main/java/com/hgx/shop/ware/service/impl/WareSkuServiceImpl.java
@@ -11,6 +11,7 @@ import com.hgx.common.utils.Query;
 import com.hgx.shop.ware.dao.WareSkuDao;
 import com.hgx.shop.ware.entity.WareSkuEntity;
 import com.hgx.shop.ware.service.WareSkuService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareSkuService")
@@ -18,9 +19,20 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if (!StringUtils.isEmpty(skuId)){
+            queryWrapper.eq("sku_id",skuId);
+        }
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)){
+            queryWrapper.eq("ware_id",wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
