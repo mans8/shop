88bdf2e6b9868ea61e56ddbf694ad7fc650ff435
小新哥 <403713133@qq.com>
2020-10-22 23:43:01 +0800

    配置ElasticSearch客户端client

---
+++ shop-search/src/main/java/com/hgx/shop/search/config/ShopElasticSearchConfig.java
@@ -0,0 +1,34 @@
+package com.hgx.shop.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hgx
+ * @date 2020/10/22 20:30
+ * @Description
+ */
+@Configuration
+public class ShopElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.1.80", 9200, "http")));
+    }
+}
