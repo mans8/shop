e4275c3c426c35b0accd29eb30ab03c3f458c7d0
小新哥 <403713133@qq.com>
2020-10-23 22:36:29 +0800

    数据传输对象

---
+++ shop-common/src/main/java/com/hgx/common/to/SkuHasStockVo.java
@@ -0,0 +1,16 @@
+package com.hgx.common.to;
+
+import lombok.Data;
+
+/**
+ * @author hgx
+ * @date 2020/10/23 15:45
+ * @Description
+ */
+@Data
+public class SkuHasStockVo {
+
+    private Long skuId;
+    private Boolean hasStock;
+
+}
