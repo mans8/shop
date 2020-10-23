adbde3ad85d40b3745fed7647a5d554f00d0dc89
小新哥 <403713133@qq.com>
2020-10-23 22:40:01 +0800

    增加ElasticSearch索引常量

---
+++ shop-search/src/main/java/com/hgx/shop/search/constant/EsConstant.java
@@ -0,0 +1,11 @@
+package com.hgx.shop.search.constant;
+
+/**
+ * @author hgx
+ * @date 2020/10/23 17:10
+ * @Description
+ */
+public class EsConstant {
+    //sku数据在ES中的索引
+    public static final String PRODUCT_INDEX = "product";
+}
