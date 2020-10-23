123d361fc3247569b6888ed18812a9e04a71cccc
小新哥 <403713133@qq.com>
2020-10-23 22:46:00 +0800

    增加sku库存量查询方法定义

--- shop-ware/src/main/java/com/hgx/shop/ware/service/WareSkuService.java
+++ shop-ware/src/main/java/com/hgx/shop/ware/service/WareSkuService.java
@@ -3,8 +3,8 @@ package com.hgx.shop.ware.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hgx.common.utils.PageUtils;
 import com.hgx.shop.ware.entity.WareSkuEntity;
-import org.apache.ibatis.annotations.Param;
-
+import com.hgx.shop.ware.vo.SkuHasStockVo;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,5 +19,7 @@ public interface WareSkuService extends IService<WareSkuEntity> {
     PageUtils queryPage(Map<String, Object> params);
 
     void addStock(Long skuId, Long wareId, Integer skuNum);
+
+    List<SkuHasStockVo> getSkuHasStock(List<Long> skuIds);
 }
 
