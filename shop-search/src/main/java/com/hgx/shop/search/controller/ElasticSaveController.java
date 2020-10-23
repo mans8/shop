0642cfcaf8145dea54258019f8ced8c768eb6b0e
小新哥 <403713133@qq.com>
2020-10-23 22:41:58 +0800

    操作ElasticSearch的Controller

---
+++ shop-search/src/main/java/com/hgx/shop/search/controller/ElasticSaveController.java
@@ -0,0 +1,48 @@
+package com.hgx.shop.search.controller;
+
+import com.hgx.common.exception.BizCodeEnume;
+import com.hgx.common.to.es.SkuEsModel;
+import com.hgx.common.utils.R;
+import com.hgx.shop.search.services.ProductSaveService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author hgx
+ * @date 2020/10/23
+ * @Description
+ */
+@Slf4j
+@RequestMapping("/search/save")
+@RestController
+public class ElasticSaveController {
+
+    @Autowired
+    ProductSaveService productSaveService;
+
+    //保存上架商品
+    @PostMapping("/product")
+    public R productStatusUp(@RequestBody List<SkuEsModel> skuEsModels) {
+
+        boolean b = false;
+        try {
+            b = productSaveService.productStatusUp(skuEsModels);
+        } catch (Exception e) {
+            log.error("ElasticSaveController商品上架错误：{}", e);
+            return R.error(BizCodeEnume.PRODUCT_UP_EXCEPTION.getCode(), BizCodeEnume.PRODUCT_UP_EXCEPTION.getMsg());
+        }
+        if (!b) {
+            return R.ok();
+        } else {
+            return R.error(BizCodeEnume.PRODUCT_UP_EXCEPTION.getCode(), BizCodeEnume.PRODUCT_UP_EXCEPTION.getMsg());
+        }
+    }
+
+
+}
