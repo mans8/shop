6a3734619feab4b16e24cf531a8d88af1d46939e
小新哥 <403713133@qq.com>
2020-10-29 16:48:34 +0800

    访问首页接口

---
+++ shop-product/src/main/java/com/hgx/shop/product/web/IndexController.java
@@ -0,0 +1,42 @@
+package com.hgx.shop.product.web;
+
+import com.hgx.shop.product.entity.CategoryEntity;
+import com.hgx.shop.product.service.CategoryService;
+import com.hgx.shop.product.vo.Catelog2Vo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author hgx
+ * @date 2020/10/25 20:20
+ * @Description
+ */
+@Controller
+public class IndexController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping({"/","/index.html"})
+    public String indexPage(Model model){
+        //TODO 1、查出所有一级分类
+        List<CategoryEntity> categoryEntities = categoryService.getLevel1Categorys();
+
+        //视图解析器进行拼串
+        model.addAttribute("categorys",categoryEntities);
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/index/catalog.json")
+    public Map<String, List<Catelog2Vo>> getCatalogJson(){
+        Map<String, List<Catelog2Vo>> catalogJson = categoryService.getCatalogJson();
+        return catalogJson;
+    }
+}
