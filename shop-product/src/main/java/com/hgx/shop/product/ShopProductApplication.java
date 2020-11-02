f173ad6233aecf03851f9226ce78d56af03cd1d7
小新哥 <403713133@qq.com>
2020-11-02 11:01:49 +0800

    增加@EnableCaching开启缓存

--- shop-product/src/main/java/com/hgx/shop/product/ShopProductApplication.java
+++ shop-product/src/main/java/com/hgx/shop/product/ShopProductApplication.java
@@ -3,6 +3,7 @@ package com.hgx.shop.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -23,6 +24,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 在application.ym中告诉sql映射文件配置
 
 */
+@EnableCaching
 @EnableFeignClients(basePackages = "com.hgx.shop.product.feign")
 @MapperScan("com.hgx.shop.product.dao")
 @EnableDiscoveryClient
