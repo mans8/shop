3df4f84ef777bbaf37e453808f24f29907fd09e3
小新哥 <403713133@qq.com>
2020-11-02 11:02:33 +0800

    测试redis客户端

--- shop-product/src/test/java/com/hgx/shop/product/ShopProductApplicationTests.java
+++ shop-product/src/test/java/com/hgx/shop/product/ShopProductApplicationTests.java
@@ -3,6 +3,7 @@ package com.hgx.shop.product;
 import com.hgx.shop.product.entity.SpuCommentEntity;
 import com.hgx.shop.product.service.SpuCommentService;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +13,9 @@ class ShopProductApplicationTests {
     @Autowired
     SpuCommentService spuCommentService;
 
+    @Autowired
+    RedissonClient redissonClient;
+
     @Test
     void contextLoads() {
         SpuCommentEntity spuCommentEntity = new SpuCommentEntity();
@@ -20,5 +24,15 @@ class ShopProductApplicationTests {
         spuCommentService.save(spuCommentEntity);
         System.out.println("保存成功");
     }
+
+    /**
+     * 测试redisson是否配置成功
+     */
+    @Test
+    public void redisson(){
+        System.out.println(redissonClient);
+    }
+
+
 
 }
