3cfe39d02103009514998c69251ff5e84f3f09f7
小新哥 <403713133@qq.com>
2020-11-07 00:28:25 +0800

    增加PRODUCT_PAGESIZE常量

--- shop-search/src/main/java/com/hgx/shop/search/constant/EsConstant.java
+++ shop-search/src/main/java/com/hgx/shop/search/constant/EsConstant.java
@@ -8,4 +8,6 @@ package com.hgx.shop.search.constant;
 public class EsConstant {
     //sku数据在ES中的索引
     public static final String PRODUCT_INDEX = "product";
+    public static final Integer PRODUCT_PAGESIZE = 2;
+
 }
