10d0f9cfa0bc6c32d7c04a38ca6163e1ba19bbe2
小新哥 <403713133@qq.com>
2020-11-20 21:16:04 +0800

    增加商品检索条件筛选联动

---
+++ shop-search/src/main/java/com/hgx/shop/search/vo/BrandVo.java
@@ -0,0 +1,14 @@
+package com.hgx.shop.search.vo;
+
+import lombok.Data;
+
+/**
+ * @author hgx
+ * @date 2020/11/20 20:51
+ * @Description
+ */
+@Data
+public class BrandVo {
+    private Long brandId;
+    private String brandName;
+}
