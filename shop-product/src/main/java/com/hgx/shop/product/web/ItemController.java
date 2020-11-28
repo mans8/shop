f6a6217d89b636899620de5e2d04b86f8368721e
小新哥 <403713133@qq.com>
2020-11-29 00:33:56 +0800

    商品详情页

---
+++ shop-product/src/main/java/com/hgx/shop/product/web/ItemController.java
@@ -0,0 +1,34 @@
+package com.hgx.shop.product.web;
+
+import com.hgx.shop.product.service.SkuInfoService;
+import com.hgx.shop.product.vo.SkuItemVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author hgx
+ * @date 2020/11/26 22:24
+ * @Description
+ */
+@Controller
+public class ItemController {
+
+    @Autowired
+    SkuInfoService skuInfoService;
+
+    /**
+     * 展示当前sku详情
+     *
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/{skuId}.html")
+    public String skuItem(@PathVariable("skuId") Long skuId, Model model) {
+        SkuItemVo vo = skuInfoService.item(skuId);
+        model.addAttribute("item",vo);
+        return "item";
+    }
+}
