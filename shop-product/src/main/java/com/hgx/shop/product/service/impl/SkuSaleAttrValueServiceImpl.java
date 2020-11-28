ed2b2d51f938d6bbdf8d045d28decbf07a043088
小新哥 <403713133@qq.com>
2020-11-29 00:37:04 +0800

    增加商品属性查询接口实现

--- shop-product/src/main/java/com/hgx/shop/product/service/impl/SkuSaleAttrValueServiceImpl.java
+++ shop-product/src/main/java/com/hgx/shop/product/service/impl/SkuSaleAttrValueServiceImpl.java
@@ -1,7 +1,11 @@
 package com.hgx.shop.product.service.impl;
 
+import com.hgx.shop.product.vo.SkuItemSaleAttrVo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +28,13 @@ public class SkuSaleAttrValueServiceImpl extends ServiceImpl<SkuSaleAttrValueDao
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<SkuItemSaleAttrVo> getSaleAttrsBySpuId(Long spuId) {
+        SkuSaleAttrValueDao dao = this.baseMapper;
+        List<SkuItemSaleAttrVo> saleAttrVos = dao.getSaleAttrsBySpuId(spuId);
+        return saleAttrVos;
     }
 
 }
