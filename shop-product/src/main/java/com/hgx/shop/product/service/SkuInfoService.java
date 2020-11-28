ee64d8f3c1ed565b1c4fc0dd32434ad9c215b275
小新哥 <403713133@qq.com>
2020-11-29 00:35:08 +0800

    增加商品查询接口

--- shop-product/src/main/java/com/hgx/shop/product/service/SkuInfoService.java
+++ shop-product/src/main/java/com/hgx/shop/product/service/SkuInfoService.java
@@ -3,6 +3,7 @@ package com.hgx.shop.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hgx.common.utils.PageUtils;
 import com.hgx.shop.product.entity.SkuInfoEntity;
+import com.hgx.shop.product.vo.SkuItemVo;
 
 import java.util.List;
 import java.util.Map;
@@ -23,5 +24,7 @@ public interface SkuInfoService extends IService<SkuInfoEntity> {
     PageUtils queryPageByCondition(Map<String, Object> params);
 
     List<SkuInfoEntity> getSkusBySpuId(Long spuId);
+
+    SkuItemVo item(Long skuId);
 }
 
