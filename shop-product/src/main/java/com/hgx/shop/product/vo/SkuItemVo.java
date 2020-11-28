c7b16233ac7b3eb60025a2d468feb18e41e37c8b
小新哥 <403713133@qq.com>
2020-11-28 16:14:52 +0800

    抽取商品详情页的数据模型

---
+++ shop-product/src/main/java/com/hgx/shop/product/vo/SkuItemVo.java
@@ -0,0 +1,54 @@
+package com.hgx.shop.product.vo;
+
+import com.hgx.shop.product.entity.SkuImagesEntity;
+import com.hgx.shop.product.entity.SkuInfoEntity;
+import com.hgx.shop.product.entity.SpuInfoDescEntity;
+import com.hgx.shop.product.entity.SpuInfoEntity;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author hgx
+ * @date 2020/11/26 22:59
+ * @Description 商品详情skuinfo
+ */
+@Data
+public class SkuItemVo {
+    //1、sku基本信息获取 pms_sku_info
+    SkuInfoEntity info;
+
+    //2、sku图片信息 pms_sku_images
+    List<SkuImagesEntity> images;
+
+    //3、获取spu的销售属性组合
+    List<SkuItemSaleAttrVo> saleAttr;
+
+    //4、获取spu介绍
+    SpuInfoDescEntity desp;
+
+    //5、获取spu规格参数信息
+    List<SpuItemAttrGroupVo> groupAttrs;
+
+
+    @Data
+    public static class SkuItemSaleAttrVo {
+        private Long attrId;
+        private String attrName;
+        private List<String> attrValues;
+    }
+
+    @Data
+    public static class SpuItemAttrGroupVo {
+        private String groupName;
+        private List<SpuBaseAttrVo> attrs;
+    }
+
+    @Data
+    public static class SpuBaseAttrVo {
+        private String attrName;
+        private String attrValue;
+    }
+
+
+}
