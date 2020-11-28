4ca3d5bda7f3286db1cfeb5ef4e332cdf533e9e5
小新哥 <403713133@qq.com>
2020-11-29 00:35:47 +0800

    增加商品分组属性查询接口

--- shop-product/src/main/java/com/hgx/shop/product/service/AttrGroupService.java
+++ shop-product/src/main/java/com/hgx/shop/product/service/AttrGroupService.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.hgx.common.utils.PageUtils;
 import com.hgx.shop.product.entity.AttrGroupEntity;
 import com.hgx.shop.product.vo.AttrGroupWithAttrsVo;
+import com.hgx.shop.product.vo.SkuItemVo;
+import com.hgx.shop.product.vo.SpuItemAttrGroupVo;
 
 import java.util.List;
 import java.util.Map;
@@ -22,5 +24,7 @@ public interface AttrGroupService extends IService<AttrGroupEntity> {
     PageUtils queryPage(Map<String, Object> params, Long catelogId);
 
     List<AttrGroupWithAttrsVo> getAttrGroupWithAttrsByCatelogId(Long catelogId);
+
+    List<SpuItemAttrGroupVo> getAttrGroupWithAttrsBySpuId(Long spuId, Long catalogId);
 }
 
