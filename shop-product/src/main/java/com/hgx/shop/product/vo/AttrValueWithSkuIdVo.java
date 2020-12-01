0c2249cb7f51b35d5ebdb3fe1d076437399bb0a9
小新哥 <403713133@qq.com>
2020-12-02 00:25:28 +0800

    sku组合切换数据vo

---
+++ shop-product/src/main/java/com/hgx/shop/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,16 @@
+package com.hgx.shop.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author hgx
+ * @date 2020/11/29 8:35
+ * @Description
+ */
+@Data
+public class AttrValueWithSkuIdVo {
+
+    private String attrValue;
+    private String skuIds;
+
+}
