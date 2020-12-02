0ec746d321a214c38b495ad49741b14b031a5292
小新哥 <403713133@qq.com>
2020-12-02 14:57:44 +0800

    sku组合切换查询接口

--- shop-product/src/main/java/com/hgx/shop/product/dao/AttrGroupDao.java
+++ shop-product/src/main/java/com/hgx/shop/product/dao/AttrGroupDao.java
@@ -2,16 +2,21 @@ package com.hgx.shop.product.dao;
 
 import com.hgx.shop.product.entity.AttrGroupEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hgx.shop.product.vo.SpuItemAttrGroupVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 属性分组
- * 
+ *
  * @author hgx
  * @email man133@126.com
  * @date 2020-10-10 13:50:13
  */
 @Mapper
 public interface AttrGroupDao extends BaseMapper<AttrGroupEntity> {
-	
+
+    List<SpuItemAttrGroupVo> getAttrGroupWithAttrsBySpuId(@Param("spuId") Long spuId, @Param("catalogId") Long catalogId);
 }
