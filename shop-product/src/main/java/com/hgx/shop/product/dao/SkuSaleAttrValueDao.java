b0f11e50f3cf1f3ce1383e84b9c8fd79a687bd97
小新哥 <403713133@qq.com>
2020-12-02 15:00:07 +0800

    根据spuid查询销售属性接口

--- shop-product/src/main/java/com/hgx/shop/product/dao/SkuSaleAttrValueDao.java
+++ shop-product/src/main/java/com/hgx/shop/product/dao/SkuSaleAttrValueDao.java
@@ -2,7 +2,11 @@ package com.hgx.shop.product.dao;
 
 import com.hgx.shop.product.entity.SkuSaleAttrValueEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hgx.shop.product.vo.SkuItemSaleAttrVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * sku销售属性&值
@@ -13,5 +17,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface SkuSaleAttrValueDao extends BaseMapper<SkuSaleAttrValueEntity> {
-	
+
+    List<SkuItemSaleAttrVo> getSaleAttrsBySpuId(@Param("spuId") Long spuId);
 }
