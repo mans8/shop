9528deff3c5800231f5e107ebf49855db67b3efb
小新哥 <403713133@qq.com>
2020-12-14 12:39:22 +0800

    登录、注册页面视图映射配置

---
+++ shop-auth-server/src/main/java/com/hgx/shop/auth/config/ShopWebConfig.java
@@ -0,0 +1,24 @@
+package com.hgx.shop.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author hgx
+ * @date 2020/12/14 12:33
+ * @Description
+ */
+
+@Configuration
+public class ShopWebConfig implements WebMvcConfigurer {
+    /**
+     * 视图映射
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/reg.html").setViewName("reg");
+    }
+}
