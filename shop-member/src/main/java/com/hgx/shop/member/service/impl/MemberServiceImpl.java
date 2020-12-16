4a27c21a9cb2f95155535979d407277d4016e4ba
小新哥 <403713133@qq.com>
2020-12-16 12:05:18 +0800

    用户注册，手机号和用户名的唯一验证

--- shop-member/src/main/java/com/hgx/shop/member/service/impl/MemberServiceImpl.java
+++ shop-member/src/main/java/com/hgx/shop/member/service/impl/MemberServiceImpl.java
@@ -1,7 +1,16 @@
 package com.hgx.shop.member.service.impl;
 
+import com.hgx.shop.member.dao.MemberLevelDao;
+import com.hgx.shop.member.entity.MemberLevelEntity;
+import com.hgx.shop.member.exception.PhoneExistException;
+import com.hgx.shop.member.exception.UsernameExistException;
+import com.hgx.shop.member.vo.MemberRegistVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +25,9 @@ import com.hgx.shop.member.service.MemberService;
 @Service("memberService")
 public class MemberServiceImpl extends ServiceImpl<MemberDao, MemberEntity> implements MemberService {
 
+    @Autowired
+    MemberLevelDao memberLevelDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<MemberEntity> page = this.page(
@@ -24,6 +36,54 @@ public class MemberServiceImpl extends ServiceImpl<MemberDao, MemberEntity> impl
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 用户注册
+     * @param vo
+     */
+    @Override
+    public void regist(MemberRegistVo vo) {
+        MemberDao memberDao = this.baseMapper;
+        MemberEntity entity = new MemberEntity();
+        //设置默认等级
+        MemberLevelEntity levelEntity = memberLevelDao.getDefaultLevel();
+        entity.setLevelId(levelEntity.getId());
+
+        //检查用户名和手机号是否唯一，为了让controller能感知异常，异常机制
+        checkPhoneUnique(vo.getPhone());
+        checkUserNameUnique(vo.getUsername());
+
+        //密码加密存储，加盐：$1$+8位字符
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encode = passwordEncoder.encode(vo.getPassword());
+        entity.setPassword(encode);
+
+        //其它默认信息
+
+
+        //设置
+        entity.setMobile(vo.getPhone());
+        entity.setUsername(vo.getUsername());
+        memberDao.insert(entity);
+    }
+
+    @Override
+    public void checkPhoneUnique(String phone) throws PhoneExistException {
+        MemberDao memberDao = this.baseMapper;
+        Integer mobile = memberDao.selectCount(new QueryWrapper<MemberEntity>().eq("mobile", phone));
+        if (mobile > 0) {
+            throw new PhoneExistException();
+        }
+    }
+
+    @Override
+    public void checkUserNameUnique(String username) throws UsernameExistException {
+        MemberDao memberDao = this.baseMapper;
+        Integer user = memberDao.selectCount(new QueryWrapper<MemberEntity>().eq("username", username));
+        if (user > 0) {
+            throw new UsernameExistException();
+        }
     }
 
 }
