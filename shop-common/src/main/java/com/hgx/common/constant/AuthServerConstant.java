952abdd72df2f983d6c09a4af4c152aa409358c1
小新哥 <403713133@qq.com>
2020-12-16 12:02:38 +0800

    统一授权服务器常量

---
+++ shop-common/src/main/java/com/hgx/common/constant/AuthServerConstant.java
@@ -0,0 +1,11 @@
+package com.hgx.common.constant;
+
+/**
+ * @author hgx
+ * @date 2020/12/14 22:20
+ * @Description
+ */
+public class AuthServerConstant {
+
+    public static final String SMS_CACHE_PREFIX = "sms:code:";
+}
