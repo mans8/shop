1e91347d7c2711480c180476559431384715dcce
小新哥 <403713133@qq.com>
2020-12-21 23:28:19 +0800

    oauth2登录接口

--- shop-auth-server/src/main/java/com/hgx/shop/auth/feign/MemberFeignService.java
+++ shop-auth-server/src/main/java/com/hgx/shop/auth/feign/MemberFeignService.java
@@ -1,6 +1,8 @@
 package com.hgx.shop.auth.feign;
 
 import com.hgx.common.utils.R;
+import com.hgx.shop.auth.vo.SocialUser;
+import com.hgx.shop.auth.vo.UserLoginVo;
 import com.hgx.shop.auth.vo.UserRegistVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,5 +18,11 @@ public interface MemberFeignService {
 
     @PostMapping("/member/member/regist")
     R regist(@RequestBody UserRegistVo vo);
+
+    @PostMapping("/member/member/login")
+    R login(@RequestBody UserLoginVo vo);
+
+    @PostMapping("/member/member/oauth2/login")
+    R oauthlogin(@RequestBody SocialUser socialUser) throws Exception;
 
 }
