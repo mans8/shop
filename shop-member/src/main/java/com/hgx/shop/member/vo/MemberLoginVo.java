3724c4c76b9caaf582d46910b99cb6f11b94a677
小新哥 <403713133@qq.com>
2020-12-21 23:29:51 +0800

    用户登录数据传输vo

---
+++ shop-member/src/main/java/com/hgx/shop/member/vo/MemberLoginVo.java
@@ -0,0 +1,16 @@
+package com.hgx.shop.member.vo;
+
+import lombok.Data;
+
+/**
+ * @author hgx
+ * @date 2020/12/16 14:27
+ * @Description
+ */
+@Data
+public class MemberLoginVo {
+    
+    private String loginacct;
+    private String password;
+
+}
