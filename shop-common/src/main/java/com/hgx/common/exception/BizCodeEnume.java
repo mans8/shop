0f1d29e258ff3c2db2a0dcde30a213b8a8a5563c
小新哥 <403713133@qq.com>
2020-12-21 23:29:22 +0800

    增加账号密码错误异常

--- shop-common/src/main/java/com/hgx/common/exception/BizCodeEnume.java
+++ shop-common/src/main/java/com/hgx/common/exception/BizCodeEnume.java
@@ -22,7 +22,8 @@ public enum BizCodeEnume {
     SMS_CODE_EXCEPTION(10002, "验证码获取频率太高，请稍后再试"),
     PRODUCT_UP_EXCEPTION(11000, "商品上架异常"),
     USER_EXIST_EXCEPTION(15001, "用户存在异常"),
-    PHONE_EXIST_EXCEPTION(15002, "手机号存在异常");
+    PHONE_EXIST_EXCEPTION(15002, "手机号存在异常"),
+    LOGINACCT_PASSWORD_INVALID_EXCEPTION(15003, "账号密码错误");
 
     private int code;
     private String message;
