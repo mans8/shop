2713042f2ce87edd67ca38dec65aea7b72db472c
小新哥 <403713133@qq.com>
2021-01-04 12:07:11 +0800

    商品服务测试

--- shop-product/src/test/java/com/hgx/shop/product/ShopProductApplicationTests.java
+++ shop-product/src/test/java/com/hgx/shop/product/ShopProductApplicationTests.java
@@ -1,11 +1,17 @@
 package com.hgx.shop.product;
 
+import com.hgx.shop.product.dao.AttrGroupDao;
+import com.hgx.shop.product.dao.SkuSaleAttrValueDao;
 import com.hgx.shop.product.entity.SpuCommentEntity;
 import com.hgx.shop.product.service.SpuCommentService;
+import com.hgx.shop.product.vo.SkuItemSaleAttrVo;
+import com.hgx.shop.product.vo.SpuItemAttrGroupVo;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ShopProductApplicationTests {
@@ -15,6 +21,12 @@ class ShopProductApplicationTests {
 
     @Autowired
     RedissonClient redissonClient;
+
+    @Autowired
+    AttrGroupDao attrGroupDao;
+
+    @Autowired
+    SkuSaleAttrValueDao skuSaleAttrValueDao;
 
     @Test
     void contextLoads() {
@@ -29,10 +41,20 @@ class ShopProductApplicationTests {
      * 测试redisson是否配置成功
      */
     @Test
-    public void redisson(){
+    public void redisson() {
         System.out.println(redissonClient);
     }
 
+    @Test
+    public void testGetAttrGroupWithAttrsBySpuId() {
+        List<SpuItemAttrGroupVo> attrGroupWithAttrsBySpuId = attrGroupDao.getAttrGroupWithAttrsBySpuId(30L, 225L);
+        System.out.println(attrGroupWithAttrsBySpuId);
+    }
 
+    @Test
+    public void testGetSaleAttrsBySpuId() {
+        List<SkuItemSaleAttrVo> saleAttrsBySpuId = skuSaleAttrValueDao.getSaleAttrsBySpuId(30L);
+        System.out.println(saleAttrsBySpuId);
+    }
 
 }
