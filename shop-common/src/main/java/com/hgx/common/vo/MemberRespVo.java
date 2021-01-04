1df73a29f1712851ed618ef7f65ca9ac8feed788
小新哥 <403713133@qq.com>
2021-01-04 11:59:48 +0800

    增加会员数据响应vo的序列化

--- shop-common/src/main/java/com/hgx/common/vo/MemberRespVo.java
+++ shop-common/src/main/java/com/hgx/common/vo/MemberRespVo.java
@@ -1,8 +1,9 @@
-package com.hgx.shop.auth.vo;
+package com.hgx.common.vo;
 
 import lombok.Data;
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -12,7 +13,10 @@ import java.util.Date;
  */
 @Data
 @ToString
-public class MemberRespVo {
+public class MemberRespVo implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
     /**
      * id
      */
