4566a877ef143b94bc3bc7781bc65fc77804abcd
小新哥 <403713133@qq.com>
2021-01-07 10:24:38 +0800

    购物车服务session配置

---
+++ shop-cart/src/main/java/com/hgx/shop/cart/config/ShopSessionConfig.java
@@ -0,0 +1,33 @@
+package com.hgx.shop.cart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * @author hgx
+ * @date 2020/1/5 21:41
+ * @Description
+ */
+@EnableRedisHttpSession
+@Configuration
+public class ShopSessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        cookieSerializer.setDomainName("shop.com");
+        cookieSerializer.setCookieName("SHOPSESSION");
+        return cookieSerializer;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+}
