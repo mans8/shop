b0c9bb728a66954c1e01d2312340843f8fa9191b
小新哥 <403713133@qq.com>
2021-01-07 10:31:17 +0800

    购物车主类

---
+++ shop-cart/src/main/java/com/hgx/shop/cart/ShopCartApplication.java
@@ -0,0 +1,18 @@
+package com.hgx.shop.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ShopCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCartApplication.class, args);
+    }
+
+}
