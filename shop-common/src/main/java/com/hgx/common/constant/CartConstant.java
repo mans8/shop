fd98770e12184fa3df00081bc9d737bac76b765c
小新哥 <403713133@qq.com>
2021-01-07 10:26:14 +0800

    购物车常量，cookie名和cookie过期时间

---
+++ shop-common/src/main/java/com/hgx/common/constant/CartConstant.java
@@ -0,0 +1,12 @@
+package com.hgx.common.constant;
+
+/**
+ * @author hgx
+ * @date 2021/1/7 21:09
+ * @Description 购物车常量
+ */
+public class CartConstant {
+
+    public static final String TEMP_USER_COOKIE_NAME = "user-key";
+    public static final int TEMP_USER_COOKIE_TIMEOUT = 60*60*24*30;
+}
