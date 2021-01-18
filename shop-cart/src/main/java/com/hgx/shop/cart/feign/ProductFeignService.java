99fc38b59e4de41fc0ea908fabc6ff7184a55039
小新哥 <403713133@qq.com>
2021-01-19 03:06:12 +0800

    购物车逻辑中feign远程调用

---
+++ shop-cart/src/main/java/com/hgx/shop/cart/feign/ProductFeignService.java
@@ -0,0 +1,25 @@
+package com.hgx.shop.cart.feign;
+
+import com.hgx.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * @author hgx
+ * @date 2021/1/7 21:33
+ * @Description
+ */
+@FeignClient("shop-product")
+public interface ProductFeignService {
+
+    @RequestMapping("/product/skuinfo/info/{skuId}")
+    R getSkuInfo(@PathVariable("skuId") Long skuId);
+
+    @GetMapping("/product/skusaleattrvalue/stringlist/{skuId}")
+    List<String> getSkuSaleAttrValues(@PathVariable("skuId") Long skuId);
+
+}
