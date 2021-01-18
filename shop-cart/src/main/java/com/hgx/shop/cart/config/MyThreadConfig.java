8a4ab6e01eb29d6243f930c19822dc4861c0fd65
小新哥 <403713133@qq.com>
2021-01-19 03:07:04 +0800

    线程池自定义配置

---
+++ shop-cart/src/main/java/com/hgx/shop/cart/config/MyThreadConfig.java
@@ -0,0 +1,33 @@
+package com.hgx.shop.cart.config;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author hgx
+ * @date 2021/1/7 0:28
+ * @Description
+ */
+@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
+@Configuration
+public class MyThreadConfig {
+
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties pool) {
+        return new ThreadPoolExecutor(
+                pool.getCoreSize(),
+                pool.getMaxSize(),
+                pool.getKeepAliveTime(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100000),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+    }
+
+}
