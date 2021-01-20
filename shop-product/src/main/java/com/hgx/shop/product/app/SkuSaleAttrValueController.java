f280152d20cc5de080d8096290ff0e2e7920c0ac
小新哥 <403713133@qq.com>
2021-01-20 12:08:23 +0800

    获取销售属性方法映射

--- shop-product/src/main/java/com/hgx/shop/product/app/SkuSaleAttrValueController.java
+++ shop-product/src/main/java/com/hgx/shop/product/app/SkuSaleAttrValueController.java
@@ -1,15 +1,12 @@
 package com.hgx.shop.product.app;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hgx.shop.product.entity.SkuSaleAttrValueEntity;
 import com.hgx.shop.product.service.SkuSaleAttrValueService;
@@ -30,6 +27,12 @@ import com.hgx.common.utils.R;
 public class SkuSaleAttrValueController {
     @Autowired
     private SkuSaleAttrValueService skuSaleAttrValueService;
+
+    @GetMapping("/stringlist/{skuId}")
+    public List<String> getSkuSaleAttrValues(@PathVariable("skuId") Long skuId){
+
+        return skuSaleAttrValueService.getSkuSaleAttrValuesAsStringList(skuId);
+    }
 
     /**
      * 列表
