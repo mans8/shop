d5963bc9b7ee70d1ca503a3bd97c618dccd06286
小新哥 <403713133@qq.com>
2021-02-01 21:25:39 +0800

    主类添加rabbit注解

--- shop-order/src/main/java/com/hgx/shop/order/ShopOrderApplication.java
+++ shop-order/src/main/java/com/hgx/shop/order/ShopOrderApplication.java
@@ -1,10 +1,24 @@
 package com.hgx.shop.order;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * 使用RabbitMQ
+ * 1.引入AMQP场景；RabbitAutoConfiguration就会自动生效
+ * 2.给容器中自动配置了
+ *     RabbitTemplate、AmqpAdmin、CachingConnectionFactory、RabbitMessagingTemplate
+ *     所有的属性都是 spring.reabbitmq
+ *     @ConfigurationProperties(prefix = "spring.rabbitmq")
+ *     public class RabbitProperties
+ * 3.在配置文件中配置spring.rabbitmq信息
+ * 4.@EnableRabbit：@EnableXxxxx  开启功能
+ */
+@EnableRabbit
 @MapperScan("com.hgx.shop.order.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
