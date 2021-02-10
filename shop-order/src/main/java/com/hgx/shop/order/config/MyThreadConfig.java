f5cbb40a2b1d9e48c742777674e0df2cf066bb74
小新哥 <403713133@qq.com>
2021-02-10 20:13:16 +0800

    订单模块线程池配置类

---
+++ shop-order/src/main/java/com/hgx/shop/order/config/MyThreadConfig.java
@@ -0,0 +1,33 @@
+package com.hgx.shop.order.config;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author hgx
+ * @date 2021/2/1 22:28
+ * @Description
+ */
+@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
+@Configuration
+public class MyThreadConfig {
+
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties pool) {
+        return new ThreadPoolExecutor(
+                pool.getCoreSize(),
+                pool.getMaxSize(),
+                pool.getKeepAliveTime(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100000),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+    }
+
+}
