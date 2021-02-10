1b84cb8226d5426dbfb5781659dd4c9d1578586e
小新哥 <403713133@qq.com>
2021-02-10 20:17:10 +0800

    配置订单模块拦截器

---
+++ shop-order/src/main/java/com/hgx/shop/order/config/OrderWebConfiguration.java
@@ -0,0 +1,24 @@
+package com.hgx.shop.order.config;
+
+import com.hgx.shop.order.interceptor.LoginUserInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author hgx
+ * @date 2021/2/10 10:37
+ * @Description 订单页拦截器
+ */
+@Configuration
+public class OrderWebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    LoginUserInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/**");
+    }
+}
