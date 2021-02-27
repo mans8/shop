739eac0e7360ee382e89960eb375d0bea1a319a7
小新哥 <403713133@qq.com>
2021-02-27 23:09:26 +0800

    订单模块feign配置

---
+++ shop-order/src/main/java/com/hgx/shop/order/config/ShopFeignConfig.java
@@ -0,0 +1,39 @@
+package com.hgx.shop.order.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author hgx
+ * @date 2021/2/26 21:24
+ * @Description
+ */
+@Configuration
+public class ShopFeignConfig {
+
+    @Bean("requestInterceptor")
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                //1.RequestContextHolder拿到刚进来的这个请求
+                ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+                //老请求
+                HttpServletRequest request = attributes.getRequest();
+                if (request != null) {
+                    //同步请求头数据，cookie
+                    String cookie = request.getHeader("Cookie");
+                    //给新请求同步了老请求的cookie
+                    requestTemplate.header("Cookie", cookie);
+                }
+            }
+        };
+    }
+
+}
