cd38b4af3090b921e4ab1cc2b58e6763d98792e0
小新哥 <403713133@qq.com>
2021-03-01 23:50:48 +0800

    根据用户收货地址计算运费接口

--- shop-ware/src/main/java/com/hgx/shop/ware/service/WareInfoService.java
+++ shop-ware/src/main/java/com/hgx/shop/ware/service/WareInfoService.java
@@ -3,6 +3,7 @@ package com.hgx.shop.ware.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hgx.common.utils.PageUtils;
 import com.hgx.shop.ware.entity.WareInfoEntity;
+import com.hgx.shop.ware.vo.FareVo;
 
 import java.util.Map;
 
@@ -16,5 +17,12 @@ import java.util.Map;
 public interface WareInfoService extends IService<WareInfoEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    /**
+     * 根据用户收货地址计算运费
+     * @param addrId
+     * @return
+     */
+    FareVo getFare(Long addrId);
 }
 
