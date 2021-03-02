258b2bac360fcb0cca7fa9e9ac4432e741e78375
小新哥 <403713133@qq.com>
2021-03-02 22:52:39 +0800

    用户下订单的防重token前缀常量

---
+++ shop-order/src/main/java/com/hgx/shop/order/constant/OrderConstant.java
@@ -0,0 +1,10 @@
+package com.hgx.shop.order.constant;
+
+/**
+ * @author hgx
+ * @date 2021/3/2 20:33
+ * @Description
+ */
+public class OrderConstant {
+    public static final String USER_ORDER_TOKEN_PREFIX = "order:token:";
+}
