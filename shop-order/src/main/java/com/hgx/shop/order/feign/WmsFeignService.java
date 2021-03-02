17c113f13cecc4c04c6681065f435d30667ca94d
小新哥 <403713133@qq.com>
2021-03-02 22:51:44 +0800

    增加获取运费和订单锁定库存接口

--- shop-order/src/main/java/com/hgx/shop/order/feign/WmsFeignService.java
+++ shop-order/src/main/java/com/hgx/shop/order/feign/WmsFeignService.java
@@ -1,9 +1,12 @@
 package com.hgx.shop.order.feign;
 
 import com.hgx.common.utils.R;
+import com.hgx.shop.order.vo.WareSkuLockVo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,4 +20,10 @@ public interface WmsFeignService {
 
     @PostMapping("/ware/waresku/hasstock")
     R getSkuHasStock(@RequestBody List<Long> skuIds);
+
+    @GetMapping("/ware/wareinfo/fare")
+    R getFare(@RequestParam("addrId") Long addrId);
+
+    @PostMapping("/ware/waresku/lock/order")
+    R orderLockStock(@RequestBody WareSkuLockVo vo);
 }
