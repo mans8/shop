9be802840b9e565329e8874cffb64f6895638556
小新哥 <403713133@qq.com>
2021-03-03 20:58:29 +0800

    增加订单确认和订单提交接口

--- shop-order/src/main/java/com/hgx/shop/order/service/OrderService.java
+++ shop-order/src/main/java/com/hgx/shop/order/service/OrderService.java
@@ -3,8 +3,12 @@ package com.hgx.shop.order.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hgx.common.utils.PageUtils;
 import com.hgx.shop.order.entity.OrderEntity;
+import com.hgx.shop.order.vo.OrderConfirmVo;
+import com.hgx.shop.order.vo.OrderSubmitVo;
+import com.hgx.shop.order.vo.SubmitOrderResponseVo;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * 订单
@@ -16,5 +20,18 @@ import java.util.Map;
 public interface OrderService extends IService<OrderEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    /**
+     * 订单确认页返回需要用的数据
+     * @return
+     */
+    OrderConfirmVo confirmOrder() throws ExecutionException, InterruptedException;
+
+    /**
+     * 下单方法
+     * @param vo
+     * @return
+     */
+    SubmitOrderResponseVo submitOrder(OrderSubmitVo vo);
 }
 
