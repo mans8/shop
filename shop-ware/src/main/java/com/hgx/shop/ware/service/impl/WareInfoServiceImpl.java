166e35904a98b0d0749a8092b4e451adadf5f0b0
小新哥 <403713133@qq.com>
2021-03-08 21:52:56 +0800

    增加根据收获地址计算运费接口实现

--- shop-ware/src/main/java/com/hgx/shop/ware/service/impl/WareInfoServiceImpl.java
+++ shop-ware/src/main/java/com/hgx/shop/ware/service/impl/WareInfoServiceImpl.java
@@ -1,7 +1,16 @@
 package com.hgx.shop.ware.service.impl;
 
+import com.alibaba.fastjson.TypeReference;
+import com.hgx.common.utils.R;
+import com.hgx.shop.ware.feign.MemberFeignService;
+import com.hgx.shop.ware.vo.FareVo;
+import com.hgx.shop.ware.vo.MemberAddressVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -17,17 +26,20 @@ import org.springframework.util.StringUtils;
 @Service("wareInfoService")
 public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity> implements WareInfoService {
 
+    @Autowired
+    MemberFeignService memberFeignService;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
 
 
         QueryWrapper<WareInfoEntity> wareInfoEntityQueryWrapper = new QueryWrapper<>();
         String key = (String) params.get("key");
-        if (!StringUtils.isEmpty(key)){
-            wareInfoEntityQueryWrapper.eq("id",key)
-                    .or().like("name",key)
-                    .or().like("address",key)
-                    .or().like("areacode",key);
+        if (!StringUtils.isEmpty(key)) {
+            wareInfoEntityQueryWrapper.eq("id", key)
+                    .or().like("name", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key);
         }
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
@@ -35,6 +47,29 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 根据收获地址计算运费
+     *
+     * @param addrId
+     * @return
+     */
+    @Override
+    public FareVo getFare(Long addrId) {
+        FareVo fareVo = new FareVo();
+        R r = memberFeignService.addrInfo(addrId);
+        MemberAddressVo data = r.getData("memberReceiveAddress", new TypeReference<MemberAddressVo>() {
+        });
+        if (data != null) {
+            String phone = data.getPhone();
+            String substring = phone.substring(phone.length() - 1, phone.length());
+            BigDecimal bigDecimal = new BigDecimal(substring);
+            fareVo.setAddress(data);
+            fareVo.setFare(bigDecimal);
+            return fareVo;
+        }
+        return null;
     }
 
 }
