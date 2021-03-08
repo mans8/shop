b26d94df90e20b3a3c606d27a2d1db17eceb6f6d
小新哥 <403713133@qq.com>
2021-03-08 21:54:36 +0800

    单点登录拦截器

---
+++ shop-order/src/main/java/com/hgx/shop/order/interceptor/LoginUserInterceptor.java
@@ -0,0 +1,36 @@
+package com.hgx.shop.order.interceptor;
+
+import com.hgx.common.constant.AuthServerConstant;
+import com.hgx.common.vo.MemberRespVo;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author hgx
+ * @date 2021/2/10 10:34
+ * @Description
+ */
+@Component
+public class LoginUserInterceptor implements HandlerInterceptor {
+
+    public static ThreadLocal<MemberRespVo> loginUser = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MemberRespVo attribute = (MemberRespVo) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
+        if (attribute != null) {
+            loginUser.set(attribute);
+            return true;
+        } else {
+            //没登录就去登录
+            request.getSession().setAttribute("msg", "请先登录");
+            response.sendRedirect("http://auth.shop.com/login.html");
+            return false;
+        }
+
+    }
+
+}
