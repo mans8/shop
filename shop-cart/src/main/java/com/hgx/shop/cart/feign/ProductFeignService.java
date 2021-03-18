e85baa98cb2c8d1319ffae9758b21175feea090d
小新哥 <403713133@qq.com>
2021-03-18 23:15:11 +0800

    获取sku价格接口

--- shop-cart/src/main/java/com/hgx/shop/cart/feign/ProductFeignService.java
+++ shop-cart/src/main/java/com/hgx/shop/cart/feign/ProductFeignService.java
@@ -22,4 +22,7 @@ public interface ProductFeignService {
     @GetMapping("/product/skusaleattrvalue/stringlist/{skuId}")
     List<String> getSkuSaleAttrValues(@PathVariable("skuId") Long skuId);
 
+    @GetMapping("/product/skuinfo/{skuId}/price")
+    R getPrice(@PathVariable("skuId") Long skuId);
+
 }
