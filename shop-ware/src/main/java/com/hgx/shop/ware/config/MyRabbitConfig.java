235113bc7b380a4d03a71cbb92d13832acac5926
小新哥 <403713133@qq.com>
2021-03-18 23:08:50 +0800

    配置库存模块的rabbitmq消息队列

---
+++ shop-ware/src/main/java/com/hgx/shop/ware/config/MyRabbitConfig.java
@@ -0,0 +1,72 @@
+package com.hgx.shop.ware.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author hgx
+ * @date 2021/3/18 21:32
+ * @Description
+ */
+@Configuration
+public class MyRabbitConfig {
+
+    /**
+     * 使用json序列化机制，进行消息转换
+     *
+     * @return
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Exchange stockEventExchange() {
+        return new TopicExchange("stock-event-exchange", true, false);
+    }
+
+    @Bean
+    public Queue stockReleaseStockQueue() {
+        return new Queue("stock.release.stock.queue", true, false, false);
+    }
+
+    @Bean
+    public Queue stockDelayQueue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "stock-event-exchange");
+        args.put("x-dead-letter-routing-key", "stock.release");
+        args.put("x-message-ttl", 120000);
+        return new Queue("stock.delay.queue", true, false, false, args);
+    }
+
+    @Bean
+    public Binding stockReleaseBinding() {
+        return new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.release.#",
+                null);
+    }
+
+    @Bean
+    public Binding stockLockedBinding() {
+        return new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "stock-event-exchange",
+                "stock.locked",
+                null);
+    }
+
+    @RabbitListener(queues = "stock.release.stock.queue")
+    public void handle(Message message){
+
+    }
+}
