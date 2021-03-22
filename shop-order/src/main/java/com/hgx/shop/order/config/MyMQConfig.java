4dafbff9b3e12583e0358d9aaddaa174813736ac
小新哥 <403713133@qq.com>
2021-03-22 23:19:48 +0800

    rabbitMQ创建队列、绑定交换机配置类

---
+++ shop-order/src/main/java/com/hgx/shop/order/config/MyMQConfig.java
@@ -0,0 +1,74 @@
+package com.hgx.shop.order.config;
+
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author hgx
+ * @date 2021/3/18 21:50
+ * @Description
+ */
+@Configuration
+public class MyMQConfig {
+
+    /**
+     * 容器中的binding queue exchange都会自动创建（rabbitMQ没有的情况）
+     *
+     * @return
+     */
+    @Bean
+    public Queue orderDelauyQueue() {
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-dead-letter-exchange", "order-event-exchange");
+        arguments.put("x-dead-letter-routing-key", "order.release.order");
+        arguments.put("x-message-ttl", 60000);
+        Queue queue = new Queue("order.delay.queue", true, false, false, arguments);
+        return queue;
+    }
+
+    @Bean
+    public Queue orderReleaseOrderQueue() {
+        Queue queue = new Queue("order.release.order.queue", true, false, false);
+        return queue;
+    }
+
+    @Bean
+    public Exchange orderEventExchange() {
+        return new TopicExchange("order-event-exchange", true, false);
+    }
+
+    @Bean
+    public Binding orderCreateOrderBinding() {
+        return new Binding("order.delay.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.create.order", null);
+    }
+
+    @Bean
+    public Binding orderReleaseOrderBinding() {
+        return new Binding("order.release.order.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.order", null);
+    }
+
+    public Binding orderReleaseOtherBinding() {
+        return new Binding("stock.release.stock.queue",
+                Binding.DestinationType.QUEUE,
+                "order-event-exchange",
+                "order.release.other.#", null);
+
+    }
+
+}
+
+
