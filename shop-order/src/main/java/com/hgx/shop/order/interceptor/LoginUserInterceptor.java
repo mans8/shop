2c1bb029ed7314b7c898dc1dadb151043723ba7c
小新哥 <403713133@qq.com>
2021-03-22 23:15:49 +0800

    登录拦截器匹配支付请求（使用了内网穿透）

--- shop-order/src/main/java/com/hgx/shop/order/interceptor/LoginUserInterceptor.java
+++ shop-order/src/main/java/com/hgx/shop/order/interceptor/LoginUserInterceptor.java
@@ -3,6 +3,7 @@ package com.hgx.shop.order.interceptor;
 import com.hgx.common.constant.AuthServerConstant;
 import com.hgx.common.vo.MemberRespVo;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,16 @@ public class LoginUserInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         MemberRespVo attribute = (MemberRespVo) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
+
+        String uri = request.getRequestURI();
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        boolean match = antPathMatcher.match("/order/order/status/**", uri);
+        //匹配支付请求（使用了内网穿透）
+        boolean match1 = antPathMatcher.match("/payed/notify", uri);
+        if (match || match1) {
+            return true;
+        }
+
         if (attribute != null) {
             loginUser.set(attribute);
             return true;
