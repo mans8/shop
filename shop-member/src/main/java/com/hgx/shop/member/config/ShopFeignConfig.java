89669bddfc003139e09f3e7208359c67c5e72093
小新哥 <403713133@qq.com>
2021-03-23 12:37:46 +0800

    feign远程配置，解决新请求丢失老请求头问题

---
+++ shop-member/src/main/java/com/hgx/shop/member/config/ShopFeignConfig.java
@@ -0,0 +1,41 @@
+package com.hgx.shop.member.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author hgx
+ * @date 2021/3/22 21:24
+ * @Description
+ */
+@Configuration
+public class ShopFeignConfig {
+
+    @Bean("requestInterceptor")
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                //1.RequestContextHolder拿到刚进来的这个请求
+                ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+                if (attributes != null) {
+                    //老请求
+                    HttpServletRequest request = attributes.getRequest();
+                    if (request != null) {
+                        //同步请求头数据，cookie
+                        String cookie = request.getHeader("Cookie");
+                        //给新请求同步了老请求的cookie
+                        requestTemplate.header("Cookie", cookie);
+                    }
+                }
+            }
+        };
+    }
+
+}
