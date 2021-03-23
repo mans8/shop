17b974c54f9318273183554f5a23cbc7e36fbaaa
小新哥 <403713133@qq.com>
2021-03-23 12:40:12 +0800

    开启redis配置

--- shop-member/src/main/java/com/hgx/shop/member/ShopMemberApplication.java
+++ shop-member/src/main/java/com/hgx/shop/member/ShopMemberApplication.java
@@ -1,11 +1,13 @@
 package com.hgx.shop.member;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@MapperScan("com.hgx.shop.member.dao")
+@EnableRedisHttpSession
+@EnableFeignClients(basePackages = "com.hgx.shop.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ShopMemberApplication {
