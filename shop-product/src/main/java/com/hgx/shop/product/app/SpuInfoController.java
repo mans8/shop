0654a9a1395055ebb69af59402773369b0dd816b
小新哥 <403713133@qq.com>
2021-03-23 12:53:36 +0800

    通过skuid获取spu

--- shop-product/src/main/java/com/hgx/shop/product/app/SpuInfoController.java
+++ shop-product/src/main/java/com/hgx/shop/product/app/SpuInfoController.java
@@ -14,7 +14,6 @@ import com.hgx.common.utils.PageUtils;
 import com.hgx.common.utils.R;
 
 
-
 /**
  * spu信息
  *
@@ -30,22 +29,28 @@ public class SpuInfoController {
 
     /**
      * 商品上架
+     *
      * @param spuId
      * @return
      */
     @PostMapping("/{spuId}/up")
-    public R spuUp(@PathVariable("spuId") Long spuId){
+    public R spuUp(@PathVariable("spuId") Long spuId) {
         spuInfoService.up(spuId);
         return R.ok();
     }
 
+    @GetMapping("/skuId/{id}")
+    public R getSpuInfoBySkuId(@PathVariable("id") Long skuId) {
+        SpuInfoEntity entity = spuInfoService.getSpuInfoBySkuId(skuId);
+        return R.ok().setData(entity);
+    }
 
     /**
      * 列表
      */
     @RequestMapping("/list")
     //@RequiresPermissions("product:spuinfo:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = spuInfoService.queryPageByCondition(params);
 
         return R.ok().put("page", page);
@@ -57,8 +62,8 @@ public class SpuInfoController {
      */
     @RequestMapping("/info/{id}")
     //@RequiresPermissions("product:spuinfo:info")
-    public R info(@PathVariable("id") Long id){
-		SpuInfoEntity spuInfo = spuInfoService.getById(id);
+    public R info(@PathVariable("id") Long id) {
+        SpuInfoEntity spuInfo = spuInfoService.getById(id);
 
         return R.ok().put("spuInfo", spuInfo);
     }
@@ -68,8 +73,8 @@ public class SpuInfoController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:spuinfo:save")
-    public R save(@RequestBody SpuSaveVo vo){
-		//spuInfoService.save(spuInfo);
+    public R save(@RequestBody SpuSaveVo vo) {
+        //spuInfoService.save(spuInfo);
         spuInfoService.saveSpuInfo(vo);
 
         return R.ok();
@@ -80,8 +85,8 @@ public class SpuInfoController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:spuinfo:update")
-    public R update(@RequestBody SpuInfoEntity spuInfo){
-		spuInfoService.updateById(spuInfo);
+    public R update(@RequestBody SpuInfoEntity spuInfo) {
+        spuInfoService.updateById(spuInfo);
 
         return R.ok();
     }
@@ -91,8 +96,8 @@ public class SpuInfoController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:spuinfo:delete")
-    public R delete(@RequestBody Long[] ids){
-		spuInfoService.removeByIds(Arrays.asList(ids));
+    public R delete(@RequestBody Long[] ids) {
+        spuInfoService.removeByIds(Arrays.asList(ids));
 
         return R.ok();
     }
