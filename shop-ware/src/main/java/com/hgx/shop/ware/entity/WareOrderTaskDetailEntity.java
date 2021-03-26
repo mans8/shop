d74ea1e9dd544304a71678bcc3c0a8028a25016e
小新哥 <403713133@qq.com>
2021-03-26 22:19:17 +0800

    优化实体类

--- shop-ware/src/main/java/com/hgx/shop/ware/entity/WareOrderTaskDetailEntity.java
+++ shop-ware/src/main/java/com/hgx/shop/ware/entity/WareOrderTaskDetailEntity.java
@@ -2,10 +2,11 @@ package com.hgx.shop.ware.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.Date;
-import lombok.Data;
 
 /**
  * 库存工作单
@@ -14,6 +15,8 @@ import lombok.Data;
  * @email man133@126.com
  * @date 2020-10-10 16:40:15
  */
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @TableName("wms_ware_order_task_detail")
 public class WareOrderTaskDetailEntity implements Serializable {
