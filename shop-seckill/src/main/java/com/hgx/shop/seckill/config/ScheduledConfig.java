86bb4ba04ab7fa1799da73452844472ccf72c915
小新哥 <403713133@qq.com>
2021-03-26 22:21:38 +0800

    秒杀模块定时配置

---
+++ shop-seckill/src/main/java/com/hgx/shop/seckill/config/ScheduledConfig.java
@@ -0,0 +1,16 @@
+package com.hgx.shop.seckill.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author hgx
+ * @date 2021/3/23 21:37
+ * @Description
+ */
+@EnableAsync
+@EnableScheduling
+@Configuration
+public class ScheduledConfig {
+}
