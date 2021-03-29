593d9066573570272d0b62b3c95e721a1a01ffef
小新哥 <403713133@qq.com>
2021-03-30 01:37:17 +0800

    @EnableRabbit监听消息加，只发送可以不加

--- shop-seckill/src/main/java/com/hgx/shop/seckill/ShopSeckillApplication.java
+++ shop-seckill/src/main/java/com/hgx/shop/seckill/ShopSeckillApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//@EnableRabbit //监听消息加，只发送可以不加
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
