ccbc4c0295287b1e9fb05419f9cbd818dcccfb4f
小新哥 <403713133@qq.com>
2021-03-30 01:35:36 +0800

    秒杀商品sku的vo对象

---
+++ shop-seckill/src/main/java/com/hgx/shop/seckill/vo/SeckillSkuVo.java
@@ -0,0 +1,45 @@
+package com.hgx.shop.seckill.vo;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author hgx
+ * @date 2021/3/24 21:16
+ * @Description
+ */
+@Data
+public class SeckillSkuVo {
+
+    private Long id;
+    /**
+     * 活动id
+     */
+    private Long promotionId;
+    /**
+     * 活动场次id
+     */
+    private Long promotionSessionId;
+    /**
+     * 商品id
+     */
+    private Long skuId;
+    /**
+     * 秒杀价格
+     */
+    private BigDecimal seckillPrice;
+    /**
+     * 秒杀总量
+     */
+    private BigDecimal seckillCount;
+    /**
+     * 每人限购数量
+     */
+    private BigDecimal seckillLimit;
+    /**
+     * 排序
+     */
+    private Integer seckillSort;
+
+}
