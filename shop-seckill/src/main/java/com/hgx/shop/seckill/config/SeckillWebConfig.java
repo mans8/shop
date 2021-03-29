46cf22c8cc15d24b6057fdf3e57796ebb8f67043
小新哥 <403713133@qq.com>
2021-03-30 01:27:54 +0800

    把拦截器添加到配置中

---
+++ shop-seckill/src/main/java/com/hgx/shop/seckill/config/SeckillWebConfig.java
@@ -0,0 +1,24 @@
+package com.hgx.shop.seckill.config;
+
+import com.hgx.shop.seckill.interceptor.LoginUserInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author hgx
+ * @date 2021/3/29 22:55
+ * @Description 把拦截器添加到配置中
+ */
+@Configuration
+public class SeckillWebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LoginUserInterceptor loginUserInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginUserInterceptor).addPathPatterns("/**");
+    }
+}
